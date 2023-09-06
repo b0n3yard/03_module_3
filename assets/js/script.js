b9423341a4e3f34a81f7199d6606b17271c4731b
unknown <b0n3yard@github.com>
2023-09-05 23:32:06 -0400

    generator works, selectors dont

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,39 @@
 // Assignment code here
-function generatePassword(){
-  var randletter = new Array('a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z');
+function generatePassword(len,rdl,rdcl,nm,spcl){
+  var running = true; 
+  var randletter = new Array('a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z');
+  var randcap =new Array('A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z');
+  var randspcl = new Array('#','$','%','&','(',')','*','+','-','.','/',':',';','<','=','>','?','@','[','\ ',']','^','_','{','|','}','~');
   var password = new Array();
-  for (let x= 0; x < 7; x++ ){
-    var choice = Math.floor(Math.random() * 2);
-    console.log(choice)
-    if (choice == 0){
+  for (let x= 0; x < len; x++ ){
+    var choice = Math.floor(Math.random() * 4);
+    
+    if (choice == 0 && nm == true){
       var num = Math.floor(Math.random() * 10);
-      console.log(num)
+      console.log(num);
       password[x] = num; 
       
-    } else if(choice == 1){
-      var newnum = Math.floor(Math.random() * 53);
+    } else if(choice == 1 && rdl == true){
+      var newnum = Math.floor(Math.random() * 27);
       var newltr =  randletter[newnum];
-      console.log(newltr)
+      console.log(newltr);
       password[x] = newltr;
+    // }else if(choice == 1 && rdl == false){
+    //    choice = choice + 1;
+    //    console.log("choice" + choice)
+      
+    // }else if(choice == 2 && spcl == true){
+      var nwnum =  Math.floor(Math.random() * 27);
+      var nwspcl = randspcl[nwnum];
+      console.log(nwspcl);
+      password[x] = nwspcl;
+      
+    } else if(choice == 3 && rdcl == true){
+      var nwnum =  Math.floor(Math.random() * 27);
+      var nwcap = randcap[nwnum];
+      console.log(nwcap);
+      password[x] = nwcap;
+
     }
   }
   console.log(password)
@@ -30,7 +49,12 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
+  var len = 9;
+  var rdl = false;
+  var rdcl = true;
+  var nm = true;
+  var spcl = true;
+  var password = generatePassword(len,rdl,rdcl,nm,spcl);
   var passwordText = document.querySelector("#password");
   
   passwordText.value = password;
