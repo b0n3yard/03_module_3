93e1e09f264b19c148d5728fc11cdf36cdbb9676
b0n3yard <namesake39@gmail.com>
2023-09-06 15:57:00 -0400

    cleaning up

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,14 +2,15 @@
 function generatePassword(len,rdl,rdcl,nm,spcl){
   var randletter = new Array('a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z');
   var randcap =new Array('A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z');
-  var randspcl = new Array('#','$','%','&','(',')','*','+','-','.','/',':',';','<','=','>','?','@','[','\ ',']','^','_','{','|','}','~');
+  var randspcl = new Array('#','$','%','&','(',')','*','+','-','.','/',':',';','<','=','>','?','@','[',']','^','_','{','|','}','~','+', ' =');
   var password = new Array();
+  
   for (let x= 0; x < len; x++ ){
     var running = true; 
     var choice = Math.floor(Math.random() * 4);
-    // console.log("inital choice: " + choice)
+    
     while (running == true){
-      // console.log("secondary choice: " + choice)
+      
       if (choice == 0 && nm == false){
         choice = choice + 1;
         
@@ -27,7 +28,7 @@ function generatePassword(len,rdl,rdcl,nm,spcl){
         choice = 0;
       } else{
         running = false;
-        // console.log("augmented choice" + choice)
+        
       }
       
     }
@@ -35,51 +36,42 @@ function generatePassword(len,rdl,rdcl,nm,spcl){
     
     if (choice == 0 && nm == true){
       var num = Math.floor(Math.random() * 10);
-      // console.log(num);
+      console.log(num)
       password[x] = num; 
       
     } else if(choice == 1 && rdl == true){
-      var newnum = Math.floor(Math.random() * 26);
+      var newnum = Math.floor(Math.random() * randletter.length);
       var newltr =  randletter[newnum];
-      // console.log(newltr);
+      console.log(newltr);
       password[x] = newltr;
-    // }else if(choice == 1 && rdl == false){
-    //    choice = choice + 1;
-    //    console.log("choice" + choice)
-      
      }else if(choice == 2 && spcl == true){
-      var nwnum =  Math.floor(Math.random() * 26);
-      var nwspcl = randspcl[nwnum];
-      // console.log(nwspcl);
-      password[x] = nwspcl;
-      
+        var nwnum =  Math.floor(Math.random() * randspcl.length);
+        var nwspcl = randspcl[nwnum];
+        console.log(nwspcl);
+        password[x] = nwspcl;
     } else if(choice == 3 && rdcl == true){
-      var nwnum =  Math.floor(Math.random() * 26);
-      var nwcap = randcap[nwnum];
-      // console.log(nwcap);
-      password[x] = nwcap;
+        var nwnum =  Math.floor(Math.random() * randcap.length);
+        var nwcap = randcap[nwnum];
+        console.log(nwcap);
+        password[x] = nwcap;
 
     }
   }
-  // console.log(password)
+  
   var running = false; 
   return password;
   
 }
 function change(val, boo){
-  console.log(val)
-  console.log(typeof val)
   if (document.getElementById(val).checked){
     var boo = true;
-    console.log("running?")
-    console.log(boo)
     return boo;
   } else{
     boo = false;
     return boo;
   }
 }
-// console.log("hello");
+
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
@@ -91,26 +83,18 @@ function writePassword() {
   console.log(len)
   var rdl = false;
   var rdcl =false;
-  nm = false;
-  spcl = false;
+  var nm = false;
+  var spcl = false;
   
   rdl = change("low", rdl);
   rdcl = change("cap", rdcl);
   nm = change("num", nm);
   spcl = change("spcl", rdl)
-  console.log(rdcl)
-  console.log(nm)
-  console.log(spcl)
-  // var rdl = document.getElementById("low").checked = true;
-  // var rdcl = document.getElementById("cap").checked = false;
-  // var nm =  document.getElementById("num").checked = false;
-  // var spcl = document.getElementById("spcl").checked = false;
   if(len <= 0 || len > 128){
     alert("invalid number")
     }else{
   if (nm == false && rdl == false && rdcl == false && spcl == false) {
-    console.log("error: all false")
-    alert("error: all false")
+    alert("error: no parameters set.")
   }else{
     var password = generatePassword(len,rdl,rdcl,nm,spcl);
     var passwordText = document.querySelector("#password");
