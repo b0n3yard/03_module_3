2cf75fb4447b7da2ce6cad32f73d0abb115a7e53
unknown <b0n3yard@github.com>
2023-09-06 01:17:34 -0400

    working on html selectors

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,15 +7,15 @@ function generatePassword(len,rdl,rdcl,nm,spcl){
   for (let x= 0; x < len; x++ ){
     var running = true; 
     var choice = Math.floor(Math.random() * 4);
-    console.log("inital choice: " + choice)
+    // console.log("inital choice: " + choice)
     while (running == true){
-      console.log("secondary choice: " + choice)
+      // console.log("secondary choice: " + choice)
       if (choice == 0 && nm == false){
         choice = choice + 1;
         
       } else if (choice == 1 && rdl == false){
         choice = choice + 1;
-        console.log("running?")
+        
         
       } else if (choice == 2 && spcl == false){
         choice = choice + 1;
@@ -27,7 +27,7 @@ function generatePassword(len,rdl,rdcl,nm,spcl){
         choice = 0;
       } else{
         running = false;
-        console.log("augmented choice" + choice)
+        // console.log("augmented choice" + choice)
       }
       
     }
@@ -35,13 +35,13 @@ function generatePassword(len,rdl,rdcl,nm,spcl){
     
     if (choice == 0 && nm == true){
       var num = Math.floor(Math.random() * 10);
-      console.log(num);
+      // console.log(num);
       password[x] = num; 
       
     } else if(choice == 1 && rdl == true){
       var newnum = Math.floor(Math.random() * 26);
       var newltr =  randletter[newnum];
-      console.log(newltr);
+      // console.log(newltr);
       password[x] = newltr;
     // }else if(choice == 1 && rdl == false){
     //    choice = choice + 1;
@@ -50,18 +50,18 @@ function generatePassword(len,rdl,rdcl,nm,spcl){
      }else if(choice == 2 && spcl == true){
       var nwnum =  Math.floor(Math.random() * 26);
       var nwspcl = randspcl[nwnum];
-      console.log(nwspcl);
+      // console.log(nwspcl);
       password[x] = nwspcl;
       
     } else if(choice == 3 && rdcl == true){
       var nwnum =  Math.floor(Math.random() * 26);
       var nwcap = randcap[nwnum];
-      console.log(nwcap);
+      // console.log(nwcap);
       password[x] = nwcap;
 
     }
   }
-  console.log(password)
+  // console.log(password)
   var running = false; 
   return password;
   
@@ -75,11 +75,16 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var len = 16;
-  var rdl = true;
-  var rdcl = true;
-  var nm = false;
-  var spcl = false;
+  var len = document.getElementById("len").value;
+  console.log(len)
+  var rdl = document.getElementById("low").checked = true;
+  console.log(rdl)
+  var rdcl = document.getElementById("cap").checked = false;
+  console.log(rdcl)
+  var nm =  document.getElementById("num").ifchecked = false;
+  console.log(nm)
+  var spcl = document.getElementById("spcl").checked = false;
+  console.log(spcl)
   var password = generatePassword(len,rdl,rdcl,nm,spcl);
   var passwordText = document.querySelector("#password");
   
